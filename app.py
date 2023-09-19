5b24f6da005435495728f63ec4765fcc9729a883
Niraj Patidar <nirajpatidar81@gmail.com>
2023-09-19 14:48:43 +0530

    removed redundant code

--- app.py
+++ app.py
@@ -24,16 +24,6 @@ async def favicon():
 async def read_item(item_id: int):
     return {"item_id": item_id}
 
-
-@app.get("/items/")
-async def list_items():
-    return [{"item_id": 1, "name": "Foo"}, {"item_id": 2, "name": "Bar"}]
-
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
-
 class InputData(BaseModel):
     data: List[str]
 
